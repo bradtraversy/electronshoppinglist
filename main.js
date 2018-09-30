c45c7d8fd2275eaf31dcffc62f4e0de23ef284df
Nico Finkernagel <nico.finkernagel@pegasus.de>
2018-09-30 09:54:34 +0200

    Added german language

--- main.js
+++ main.js
@@ -5,23 +5,28 @@ const url = require('url');
 // SET ENV
 process.env.NODE_ENV = 'development';
 
-const {app, BrowserWindow, Menu, ipcMain} = electron;
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  ipcMain
+} = electron;
 
 let mainWindow;
 let addWindow;
 
 // Listen for app to be ready
-app.on('ready', function(){
+app.on('ready', function() {
   // Create new window
   mainWindow = new BrowserWindow({});
   // Load html in window
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'mainWindow.html'),
     protocol: 'file:',
-    slashes:true
+    slashes: true
   }));
   // Quit app when closed
-  mainWindow.on('closed', function(){
+  mainWindow.on('closed', function() {
     app.quit();
   });
 
@@ -32,53 +37,52 @@ app.on('ready', function(){
 });
 
 // Handle add item window
-function createAddWindow(){
+function createAddWindow() {
   addWindow = new BrowserWindow({
     width: 300,
-    height:200,
-    title:'Add Shopping List Item'
+    height: 200,
+    title: 'Füge einen Artikel hinzu'
   });
   addWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'addWindow.html'),
     protocol: 'file:',
-    slashes:true
+    slashes: true
   }));
   // Handle garbage collection
-  addWindow.on('close', function(){
+  addWindow.on('close', function() {
     addWindow = null;
   });
 }
 
 // Catch item:add
-ipcMain.on('item:add', function(e, item){
+ipcMain.on('item:add', function(e, item) {
   mainWindow.webContents.send('item:add', item);
-  addWindow.close(); 
+  addWindow.close();
   // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
   //addWindow = null;
 });
 
 // Create menu template
-const mainMenuTemplate =  [
+const mainMenuTemplate = [
   // Each object is a dropdown
   {
-    label: 'File',
-    submenu:[
-      {
-        label:'Add Item',
-        click(){
+    label: 'Datei',
+    submenu: [{
+        label: 'Artikel hinzufügen',
+        click() {
           createAddWindow();
         }
       },
       {
-        label:'Clear Items',
-        click(){
+        label: 'Alle Artikel löschen',
+        click() {
           mainWindow.webContents.send('item:clear');
         }
       },
       {
-        label: 'Quit',
-        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-        click(){
+        label: 'Verlassen',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
           app.quit();
         }
       }
@@ -87,25 +91,24 @@ const mainMenuTemplate =  [
 ];
 
 // If OSX, add empty object to menu
-if(process.platform == 'darwin'){
+if (process.platform == 'darwin') {
   mainMenuTemplate.unshift({});
 }
 
-// Add developer tools option if in dev
-if(process.env.NODE_ENV !== 'production'){
-  mainMenuTemplate.push({
-    label: 'Developer Tools',
-    submenu:[
-      {
-        role: 'reload'
-      },
-      {
-        label: 'Toggle DevTools',
-        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-        click(item, focusedWindow){
-          focusedWindow.toggleDevTools();
-        }
-      }
-    ]
-  });
-}
+// // Add developer tools option if in dev
+// if (process.env.NODE_ENV !== 'production') {
+//   mainMenuTemplate.push({
+//     label: 'Developer Tools',
+//     submenu: [{
+//         role: 'reload'
+//       },
+//       {
+//         label: 'Toggle DevTools',
+//         accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+//         click(item, focusedWindow) {
+//           focusedWindow.toggleDevTools();
+//         }
+//       }
+//     ]
+//   });
+// }
